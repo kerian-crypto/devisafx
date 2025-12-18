2cd2197747e11d6cc92e9a0e7e7baad66792466c
kerian-crypto <nklorion69@gmail.com>
2025-12-18 09:27:40 +0100

    Update main.py

--- FINAL/main.py
+++ FINAL/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 import hashlib
@@ -49,6 +50,7 @@ def health_check():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
 
