aa18f58fc918f619ef3953404f97a37610e195f4
kerian-crypto <nklorion69@gmail.com>
2025-12-22 14:32:45 +0100

    Delete FINAL/migrations_runtime.py

--- FINAL/migrations_runtime.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import text
-from app import db  # adapte si ton db est ailleurs
-
-def update_numero_marchand_length():
-    with db.engine.begin() as connection:
-        connection.execute(text("""
-            ALTER TABLE transactions
-            ALTER COLUMN numero_marchand TYPE VARCHAR(100)
-        """))
